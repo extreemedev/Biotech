2b4a1e9da46df9f646605113de99230d12b8edea
extreemedev <mattallev@gmail.com>
2022-09-22 18:14:23 +0200

    pipe in python

---
+++ scripts/pathstart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="home/headless/BioPrograms/SPAdes-3.14.1/bin/:$PATH"
+
+export PATH="home/headless/BioPrograms/FastQC/fastqc:$PATH"
---
+++ scripts/pipe.py
@@ -0,0 +1,11 @@
+import os
+
+pip = os.system("sudo apt update && sudo apt install -y python3-pip")
+print(pip)
+dockerinst = os.system("pip install docker")
+print(dockerinst)
+# You could also use the os.popen() method
+#with os.popen("ls -lh") as f:
+    #print(f.readlines())
+
+import docker

ff9b3ea1b4b4375bfa1f4fb295a7356f98b9be31
extreemedev <mattallev@gmail.com>
2022-09-23 15:26:26 +0200

    pipe alert

--- scripts/FileChooser.py
+++ scripts/FileChooser.py
@@ -1,3 +1,11 @@
+# ---------------------------------------------------------------------------- #
+#                                    Easygui                                   #
+# ---------------------------------------------------------------------------- #
+
 import easygui
 
-path = easygui.fileopenbox()
+start = easygui.msgbox("Fully automated Pipeline for Assembly Annotation and Building.\n\nPlease press the 'Accept' button to select the starting file of this Pipeline.","Assembly Pipeline","Accept", "pipe_workflow.png")
+if (start == "Accept" or start == "pipe_workflow.png"):
+    path = easygui.fileopenbox("Seleziona il file","Pipe")
+
+print("\n"+path+"\n")

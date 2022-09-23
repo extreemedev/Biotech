0bfc84fbb3a9ff6bddf74ffc48500cba84e34b06
extreemedev <mattallev@gmail.com>
2022-09-23 19:40:39 +0200

    pipe spades

--- scripts/pipeFull.py
+++ scripts/pipeFull.py
@@ -25,27 +25,28 @@ print(msg_fastqc)
 
 trimmomatic = client.containers.get('trimmomatic')
 comm = "trimmomatic PE -threads 4 SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
-    SRR2589044_1.trim.fastq.gz SRR2589044_1un.trim.fastq.gz \
-    SRR2589044_2.trim.fastq.gz SRR2589044_2un.trim.fastq.gz \
+    SRR2589044_1.trim.fastq.gz SRR2589044_1.untrim.fastq.gz \
+    SRR2589044_2.trim.fastq.gz SRR2589044_2.untrim.fastq.gz \
     ILLUMINACLIP:/Trimmomatic-0.39/adapters/NexteraPE-PE.fa:2:40:15 SLIDINGWINDOW:4:20 MINLEN:25"
-#std_comm = 'java -jar /Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 60 -phred33 BP_87_1.fq.gz BP_87_2.fq.gz \
-# BP_87_1.trimmed.paired.fastq BP_87_1.trimmed.unpaired.fastq BP_87_2.trimmed.paired.fastq BP_87_2.trimmed.unpaired.fastq 
-# \ ILLUMINACLIP:/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:30:10 SLIDINGWINDOW:4:15 MINLEN:36 HEADCROP:13'
+
 msg_trimmomatic = trimmomatic.exec_run(comm, stdout=True, stderr=True, stdin=False, tty=False, privileged=False, 
  user='', detach=False, stream=False, socket=False, environment=None, workdir=None, demux=False)
 print(msg_trimmomatic)
+
+move = os.system("mkdir ./Trimmomatic Trimmomatic/trimmed_fastq Trimmomatic/untrimmed_fastq && mv *.trim* Trimmomatic/trimmed_fastq && mv *.untrim* Trimmomatic/untrimmed_fastq")
 
 
 
 # ---------------------------------------------------------------------------- #
 #                                 SPAdes-3.14.1                                #
 # ---------------------------------------------------------------------------- #
-'''
+
+
 spades = client.containers.get("spades")
 print(spades)
-msg_spades = spades.exec_run("python /SPAdes-3.15.4-Linux/bin/rnaspades.py" , stdout=True, stderr=True, stdin=False, tty=False, privileged=False, user='', detach=False, stream=False, socket=False, environment=None, workdir=None, demux=False)
+msg_spades = spades.exec_run("python /SPAdes-3.15.4-Linux/bin/rnaspades.py -1 Trimmomatic/trimmed_fastq/SRR2589044_1.trim.fastq.gz -2 Trimmomatic/trimmed_fastq/SRR2589044_2.trim.fastq.gz -o Spades " , stdout=True, stderr=True, stdin=False, tty=False, privileged=False, user='', detach=False, stream=False, socket=False, environment=None, workdir=None, demux=False)
 print(msg_spades)
-'''
+
 
 
 # ---------------------------------------------------------------------------- #

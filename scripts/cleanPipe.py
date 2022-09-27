56f6eca730b6209db5885b3c963e1d560e129766
Adri <adrianogiuliani0@gmail.com>
2022-09-27 19:39:00 +0200

    Clean pipe and install pipe

--- scripts/cleanPipe.py
+++ scripts/cleanPipe.py
@@ -1,12 +1,12 @@
 import os
 
 print("\n")
-pathlist = "Trimmomatic", "Spades", "cdhitest", "busco"
+pathlist = "Trimmomatic", "Spades", "cdhitest", "busco", "__pycache__"
 for i in pathlist:
     if os.path.exists(i):
         remove = os.system("rm -rf ./"+i)
         if (remove==0):
             print("\n"+i+" directory successfully removed!\n")
     else:
-        print("\n"+i+" directory have been removed yet.\n")
+        print("\n"+i+" directory has already been removed.\n")
 print("\n")
--- scripts/pipeInstall.py
+++ scripts/pipeInstall.py
@@ -3,6 +3,11 @@ import os
 pip = os.system("sudo apt update && sudo apt install -y python3-pip")
 print(pip)
 
+tk = os.system("sudo apt-get update && sudo apt-get install -y python3-tk")
+print(tk)
+
 dockerinst = os.system("pip install docker")
 print(dockerinst)
 
+alive = os.system("pip install alive-progress")
+print(alive)

f29556eab27a3fd1e1bae1cd4a3e73e97a061a5f
extreemedev <mattallev@gmail.com>
2023-02-22 17:20:20 +0100

    test deploy 2

--- utils/pipeManager/pipeInstall.py
+++ utils/pipeManager/pipeInstall.py
@@ -5,6 +5,7 @@ import os
 piptk = os.system("sudo apt-get update && sudo apt-get install -y python3-pip")
 dockerinst = os.system("sudo pip install docker")
 alive = os.system("sudo pip install alive-progress")
+util = os.system("sudo pip install psutil")
 
 # ------------------------------- WORKDIR TREE ------------------------------- #
 
@@ -15,7 +16,7 @@ if workdir[-1] != "/":
 for i in ("/opt/pipeline","/opt/pipeline/bin","/opt/pipeline/etc","/opt/pipeline/var","/opt/pipeline/log","/opt/pipeline/opt","/opt/pipeline/lib"):
      if not os.path.isdir(i):
           mkdir = os.system("sudo mkdir "+i)
-          print("\nDirectory successfully created!\n") if (int(mkdir) == 0) else print("Something went wrong when creating the directory. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
+          print("Directory successfully created.\n") if (int(mkdir) == 0) else print("Something went wrong when creating the directory. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
 mod = os.system("sudo chmod -R 777 /opt/pipeline/")
 print("Workdir tree successfully granted.") if (int(mod) == 0) else print("Something went wrong when granting permission. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
 
@@ -37,6 +38,13 @@ print("Pipeline libraries successfully deployed.") if (int(pipestat) == 0) else 
 # ---------------------------------- SERVICE --------------------------------- #
 
 pipeline_service = os.system("sudo cp "+workdir+"/utils/pipeManager/pipeline /etc/init.d/pipeline")
-print("Pipeline service successfully copied.") if (int(pipeline_service) == 0) else print("Something went wrong when copying the service file. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
+print("Pipeline service successfully copied.(Systemd)") if (int(pipeline_service) == 0) else print("Something went wrong when copying the service file. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
 pipeline_service = os.system("sudo chmod 755 /etc/init.d/pipeline")
+print("Pipeline service successfully granted.") if (int(pipeline_service) == 0) else print("Something went wrong when granting permission. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
+
+# --------------------------------- SYSTEMCTL -------------------------------- #
+
+pipeline_sysctl = os.system("sudo cp "+workdir+"/utils/pipeManager/pipeline.service /etc/systemd/system/pipeline.service")
+print("Pipeline service successfully copied.(Systemctl)") if (int(pipeline_service) == 0) else print("Something went wrong when copying the service file. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
+pipeline_sysctl = os.system("sudo chmod 755 /etc/systemd/system/pipeline.service")
 print("Pipeline service successfully granted.") if (int(pipeline_service) == 0) else print("Something went wrong when granting permission. If the problem persists please read the documentation here: https://github.com/extreemedev/Biotech/blob/master/README.md\n")
--- utils/pipeManager/pipeUninstall.py
+++ utils/pipeManager/pipeUninstall.py
@@ -5,7 +5,16 @@ import os
 piptk = os.system("sudo apt-get update && sudo apt-get remove -y python3-tk")
 dockerinst = os.system("sudo pip remove docker")
 alive = os.system("sudo pip remove alive-progress")
+util = os.system("sudo pip remove psutil")
 
 # ------------------------------- WORKDIR TREE ------------------------------- #
 
 os.system("sudo rm -r /opt/pipeline/")
+
+# ---------------------------------- SERVICE --------------------------------- #
+
+pipeline_service = os.system("sudo rm /etc/init.d/pipeline")
+
+# --------------------------------- SYSTEMCTL -------------------------------- #
+
+pipeline_sysctl = os.system("sudo rm /etc/systemd/system/pipeline.service")
